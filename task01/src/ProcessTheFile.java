6d7f6d53d11897c8fdd99c6c176ad2b61f840de9
Haijie95 <haijie95@gmail.com>
2022-12-16 16:55:50 +0800

    Final Submmission

--- task01/src/ProcessTheFile.java
+++ task01/src/ProcessTheFile.java
@@ -1,21 +1,42 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.Reader;
-import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class ProcessTheFile {
 
     private String fileName;
-    private List<String> firstName;
-    private List<String> lastName;
-    private List<String> address;
-    private List<String> years;
+    private String parameter1="";
+    private String parameter2="";
+    private String parameter3="";
+    private String parameter4="";
+    private Map<String,String> allParameters= new HashMap<>();
 
+    //constructor
     public ProcessTheFile(String fileName) {
         this.fileName = fileName;
     }
+
+    //getter
+    public String getParameter1() {
+        return parameter1;
+    }
+
+    public String getParameter2() {
+        return parameter2;
+    }
+
+    public String getParameter3() {
+        return parameter3;
+    }
+
+    public String getParameter4() {
+        return parameter4;
+    }
+    
 
     public void start() {
 
@@ -24,21 +45,28 @@ public class ProcessTheFile {
             BufferedReader br = new BufferedReader(r);
             
             String line;
-
+            Integer count=0;
             while (null != (line = br.readLine())) {
-                
                 String[] info = line.trim().split(",");
-                firstName.add(info[0].toString());
-                lastName.add(info[1].toString());
-                address.add(info[2].toString());
-                years.add(info[3].toString());
+                if(count==0){
+                    parameter1=info[0];
+                    parameter2=info[1];
+                    parameter3=info[2];
+                    parameter4=info[3];
+                } else{
+                    parameter1=parameter1+"  "+info[0];
+                    parameter2=parameter2+"  "+info[1];
+                    parameter3=parameter3+"  "+info[2];
+                    parameter4=parameter4+"  "+info[3];
+                }
+                count++;
             }
-            System.out.println(firstName);
-
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    
 
 }
